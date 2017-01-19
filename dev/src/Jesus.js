0213cd83de314cf08dd68eb50a16e1fe1bda26a6
Matheus Blödorn <blodorn.matheus@gmail.com>
2017-01-19 17:32:41 -0200

    Jesus.save()

--- dev/src/Jesus.js
+++ dev/src/Jesus.js
@@ -39,12 +39,12 @@
 	Jesus.save = function(){
 		//Jesus is able to save everyone (in .txt)
 		//more than once because Jesus
-		var btnSaveEveryone = document.getElementById('save-everyone');
-		if(!btnSaveEveryone){
+		var saveEveryone = document.getElementById('save-everyone');
+		if(!saveEveryone){
 			document.body.innerHTML += '<a style="display: none;" id="save-everyone" href="data:application/xml;charset=utf-8, everyone" download="everyone.txt"></a>';
-			btnSaveEveryone = document.getElementById('save-everyone');
+			saveEveryone = document.getElementById('save-everyone');
 		}
-		btnSaveEveryone.click();
+		saveEveryone.click();
 	}
 
     exports.Jesus = Jesus;
